213c5c6d3d53e8c61072bba7125b5d00985dc8f8
zhao1411935907 <Zihe.Zhao@lincolnuni.ac.nz>
2024-10-14 22:58:12 +1300

    1

---
+++ connect.py
@@ -0,0 +1,7 @@
+# These details are available on the first MySQL Workbench screen
+# Usually called 'Local Instance'
+dbuser = "root" # Your MySQL username - likely 'root'
+dbpass = "zhao1411935907" # ---- PUT YOUR PASSWORD HERE ----
+dbhost = "localhost" 
+dbport = "3306"
+dbname = "drylandapp"
